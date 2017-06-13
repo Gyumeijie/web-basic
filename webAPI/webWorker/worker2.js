27a2fa827cdf30a1ffb221383d23e28ad2c37acb
Gyumeijie <Gyumeijie@gmail.com>
2017-06-13 20:47:37 +0800

    add web worker example

---
+++ webAPI/webWorker/worker2.js
@@ -0,0 +1,17 @@
+/**
+ * Created by nick on 17-6-13.
+ */
+
+self.onmessage = function(event){
+    console.log("accept message")
+    var data = event.data;
+    data.sort(function(a,b){
+        return b-a;
+    });
+    self.postMessage(data);
+
+}
+
+self.onerror = function(event){
+    console.log("error in worker")
+}
